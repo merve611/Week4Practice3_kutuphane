6c13e4794bd3a5aa8a65a5e2eba747bcb006d5da
merve cakiroglu <mrveeckrrr@hotmail.com>
2024-08-06 17:02:54 +0300

    Add project files.

---
+++ Week4Practice3_kutuphane/Books.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4Practice3_kutuphane
+{
+    public class Books
+    {
+        private string _bookName;
+        private string _authorName;                  //yazar ismi
+        private string _authorLastname;              //yazar soyismi
+        private int _numberofPages;                  //sayfa sayısı
+        private string _publiser;                    //yayınevi
+        private DateTime _dateofRegistration;       //kayıt tarihi
+
+        public Books()                          //Parametre almayan ctor
+        {
+            DateRegistration();
+
+        }
+        
+        public Books(string bookName, string authorName,string authorLastname, int numberofPages, string publiser )               //Parametreli ctor
+        {
+            _bookName = bookName;
+            _authorName = authorName;
+            _authorLastname = authorLastname;
+            _numberofPages = numberofPages;
+            _publiser = publiser;
+            
+            DateRegistration();
+
+        }
+
+        public void DateRegistration()                                          //Kayıt tarihinin oluşturulması için metot
+        {
+            _dateofRegistration = DateTime.Now;
+            Console.WriteLine("Kayıt tarihi : "+ _dateofRegistration);
+        }
+
+        public void AddBooks()                                                  //Kitap kaydetme metodu
+        {
+            Console.WriteLine(_authorName + " "+ _authorLastname +  " 'in " + _numberofPages + " sayfalık, " + _publiser + " yayınevinden çıkan " + _bookName + " kitabi kaydedildi.");
+        }
+
+
+        public string BookName { get { return _bookName; } set { _bookName = value; } }
+        public string AuthorName { get { return _authorName; } set { _authorName = value; } }
+        public string AuthorLastName { get { return _authorLastname; } set { _authorLastname = value; } }
+        public int NumberOfPages { get { return _numberofPages; } set { _numberofPages = value; } }
+        public string Publiser { get { return _publiser; } set { _publiser = value; } }
+        public DateTime DateOfRegistration { get { return _dateofRegistration; } set { _dateofRegistration = value; } }
+
+
+
+
+    }
+}
---
+++ Week4Practice3_kutuphane/Program.cs
@@ -0,0 +1,29 @@
+﻿using Week4Practice3_kutuphane;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("-------Parametresiz ctor----------");
+
+        Books book1 = new Books();
+        book1.AuthorName = "Merve";
+        book1.AuthorLastName = "Çakıroğlu";
+        book1.BookName = "Alaca Geyik";
+        book1.NumberOfPages = 450;
+        book1.Publiser = "Kültür yayınları";
+        book1.AddBooks();
+
+
+
+        Console.WriteLine("-------Parametreli ctor-----------");
+        Books book2 = new Books("Adı Aylin","Ayşe","Kulin",398,"Remzi Kitapevi");
+        book2.AddBooks();
+    }
+}
+
+
+//Class ; OOP'nin temel yapıtaşlarından olan class,nesnelerin özelliklerini ve davranışlarını tanımlayan bir şablondur. 
+//Property ; Bir sınıfın alanlarına (fields) erişim sağlamak ve bu alanları korumak için kullanılan üyeleridir.
+//New ; Bir sınıftan yeni bir nesne oluşturmak için kullanılır. 
+//Constructor ; Sınıftan yeni bir nesne oluşturulduğunda çağrılan özel bir metottur. Constructor'lar, nesne oluşturulurken gerekli başlangıç değerlerini ayarlamak için kullanılır. 
